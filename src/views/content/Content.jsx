1a131c009eea9ffcc81644f4f050fb7d38f6e0d6
DevSirHitsuji <gustavooficial2511@gmail.com>
2023-01-17 14:53:52 -0300

    save in local storage

--- src/views/content/Content.jsx
+++ src/views/content/Content.jsx
@@ -1,6 +1,6 @@
 import "./Content.css"
 
-import axios from 'axios'
+
 import { useEffect, useState } from 'react'
 
 import Preview from '../../components/preview/Preview'
@@ -13,30 +13,22 @@ import MyFooter from '../../components/myFooter/MyFooter'
 
 export default function Content(props) {
 
-    const [title, setTitle] = useState();
-    const [info, setInfo] = useState();
-    const [imgUrl, setImgUrl] = useState();
+    // const [title, setTitle] = useState();
+    // const [info, setInfo] = useState();
+    // const [imgUrl, setImgUrl] = useState();
   
     
-    const getDate = async () => {
-      const url = "https://api.nasa.gov/planetary/apod?api_key=" + "kdfNSXeGP6S3lB4RxgB05UXhgxNPIR850imFrlDB" + "&date=" + localStorage.getItem("date");
-      const res = await axios.get(url);
-      const data = await res.data;
-      setTitle(data.title)
-      setInfo(data.explanation)
-      setImgUrl(data.url)
-    }
-    useEffect(() => getDate, [])
+
 
     return (
         <div>
         <MyHeader tittle="Universe For All" />
       
         <main>
-            <Preview src={imgUrl}/>
+            <Preview src={localStorage.getItem("url")}/>
             <Info 
-            tittle={title}
-            description={info}
+            tittle={localStorage.getItem("title")}
+            description={localStorage.getItem("explanation")}
             />
         </main>
 
--- src/views/putInfo/PutInfo.jsx
+++ src/views/putInfo/PutInfo.jsx
@@ -5,9 +5,21 @@ import Input from "../../components/input/Input"
 import MyHeader from "../../components/MyHeader/MyHeader"
 import MyFooter from "../../components/myFooter/MyFooter"
 
-import { Link } from "react-router-dom"
+import {useNavigate } from "react-router-dom"
+import axios from 'axios'
 
 export default function PutInfo(props) {
+    const navigate = useNavigate();
+    const getDate = async () => {
+        const url = "https://api.nasa.gov/planetary/apod?api_key=" + "kdfNSXeGP6S3lB4RxgB05UXhgxNPIR850imFrlDB" + "&date=" + localStorage.getItem("date");
+        const res = await axios.get(url);
+        const data = await res.data;
+        localStorage.setItem("title", data.title)
+        localStorage.setItem("explanation", data.explanation)
+        localStorage.setItem("url", data.url)  
+        navigate("/content")
+      }
+
     return (
         <div>
         <MyHeader tittle="Universe For All" />
@@ -21,7 +33,7 @@ export default function PutInfo(props) {
             placeholder="choice a date..."
             func={(e) => {localStorage.setItem("date", e.target.value)}}
         />
-        <button ><Link to="/content">continue</Link></button>
+        <button onClick={getDate}>continue</button>
         
         <MyFooter/>
         </div>       
